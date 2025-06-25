22d7d7592709667274b8fe628bbd0dae3c835534
SSSwetha25 <144270599+SSSwetha25@users.noreply.github.com>
2025-06-25 23:18:55 +0530

    Chat page - contacts column ui

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,7 +1,15 @@
 import chat from './chat.png'
 import background1 from './background1.jpg'
+import profile_img from './profile_img.jpg'
+import menu_icon from './menu_icon.png'
+import search_icon from './search_icon.png'
+import logo from './logo.png'
 const assets={
  chat,
- background1
+ background1,
+ profile_img,
+ menu_icon,
+ search_icon,
+ logo
 }
 export default assets;
--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import './Chat.css'
+import LeftSideBar from '../../components/LeftSideBar/LeftSideBar'
+import ChatBox from '../../components/ChatBox/ChatBox'
+import RightSideBar from '../../components/RightSideBar/RightSideBar'
 const Chat = () => {
   return (
-    <div>we are on the chat page</div>
+    <div className='chat'>
+    <div className="chat-container">
+      <LeftSideBar />
+      <ChatBox />
+      <RightSideBar/>
+      </div>  
+    </div>
   )
 }
 
