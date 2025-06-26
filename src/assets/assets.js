f12027e4529fa7557bef3984111d0dc32145fd7b
SSSwetha25 <144270599+SSSwetha25@users.noreply.github.com>
2025-06-26 23:26:18 +0530

    chat section ui

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -4,12 +4,20 @@ import profile_img from './profile_img.jpg'
 import menu_icon from './menu_icon.png'
 import search_icon from './search_icon.png'
 import logo from './logo.png'
+import green_dot from './green_dot.png'
+import help_icon from './help_icon.png'
+import send_icon from './send_icon.png'
+import gallery_icon from './gallery_icon.png'
 const assets={
  chat,
+ send_icon,
+ gallery_icon,
+ help_icon,
  background1,
  profile_img,
  menu_icon,
  search_icon,
- logo
+ logo,
+ green_dot
 }
 export default assets;
