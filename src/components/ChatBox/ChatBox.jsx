238a618c404b77b589f5838c625f74a392b13999
SSSwetha25 <144270599+SSSwetha25@users.noreply.github.com>
2025-06-28 22:53:06 +0530

    chat msgs ui

--- src/components/ChatBox/ChatBox.jsx
+++ src/components/ChatBox/ChatBox.jsx
@@ -9,6 +9,24 @@ const ChatBox = () => {
         <p>Alison Swift <img className="dot" src={assets.green_dot} alt="" /></p>
         <img src={assets.help_icon} className='help' alt="" />
         </div>
+
+        <div className="chat-msg">
+          <div className="s-msg">
+            <p className="msg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque vitae sequi non.</p>
+            <div>
+              <img src={assets.profile_img} width="100px" alt="" />
+              <p>2:30 PM</p>
+            </div>
+          </div>
+          <div className="r-msg">
+            <p className="msg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque vitae sequi non.</p>
+            <div>
+              <img src={assets.profile_img} width="100px" alt="" />
+              <p>2:30 PM</p>
+            </div>
+          </div>
+        </div>
+        
         <div className="chat-input">
           <input type="text" placeholder='Send a message' />
           <input type="file" id='image' accept='image/png, image/jpeg' hidden />
