74e81a3e303c2cfddce87e51b7b31a90d55c86fa
SSSwetha25 <144270599+SSSwetha25@users.noreply.github.com>
2025-06-29 23:58:03 +0530

    rightside bar ui

--- src/components/RightSideBar/RightSideBar.jsx
+++ src/components/RightSideBar/RightSideBar.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
 import './RightSideBar.css'
+import assets from '../../assets/assets'
 const RightSideBar = () => {
   return (
-    <div>RightSideBar</div>
+    <div className='rs'>
+      <div className="rs-profile">
+        <img src={assets.profile_img} width="100px" alt="" />
+        <h3>Alison Swift<img src={assets.green_dot} className='dot' alt="" /></h3>
+        <p>Hey there I'm Alison swift </p>
+        </div>
+        <hr/>
+        <div className="rs-media">
+          <p>Media</p>
+          <div>
+            <img src={assets.pic} alt="" />
+            <img src={assets.pic} alt="" />
+            <img src={assets.pic} alt="" />
+            <img src={assets.pic} alt="" />
+            <img src={assets.pic} alt="" />
+            <img src={assets.pic} alt="" />
+          </div>
+        </div>
+        <button>Logout</button>
+        RightSideBar</div>
   )
 }
 
